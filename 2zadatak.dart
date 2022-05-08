c589a4a7305f66127ea462d8925bad0b20b0c3a6
nbusic <busic.nikolina@gmail.com>
2022-05-08 14:34:14 +0200

    2 exercise

---
+++ 2zadatak.dart
@@ -0,0 +1,9 @@
+double temperature = 25;
+int value = 3;
+String pizza = "pizza";
+String pasta = "pasta";
+void main() {
+print ("the temperature is $temperature C");
+print ("$value plus $value makes ${value + value}");
+print ("I like $pizza and $pasta");
+  }
